bd45ffb15ee781c9f733392e6deff109fd677496
tazzan <tazzanyt@gmail.com>
2025-04-17 01:03:17 -0400

    Initial commit

---
+++ ChakraCollection/localization/en_us.lua
@@ -0,0 +1,24 @@
+return {
+    descriptions = {
+        Edtion = {
+            e_chak_ignited = {
+                name = "Ignited",
+                text = {
+                    'Retriggers itself, {C:green,s:0.8}1 in 6',
+                    'chance this card is {C:red}destroyed',
+                    'at end of round'
+                }
+            }
+        },
+        Other = {
+            chak_ethereal = {
+                name = "Ethereal",
+                text = {
+                    'Disables itself when',
+                    'you enter the shop,',
+                    'always has {C:orange}$0{} sell value'
+                }
+            }
+        }
+    }
+}
---
+++ ChakraCollection/smods.lua
@@ -0,0 +1,10 @@
+-- Steamodded is not necessary. This just adds a bit of compatibility.
+
+if SMODS.Atlas then
+    SMODS.Atlas({
+        key = "modicon",
+        path = "modicon.png",
+        px = 34,
+        py = 34
+    })
+end
